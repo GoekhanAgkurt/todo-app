a26e3c8acb546cc38ce7986f276034f0d265239f
Goekhan Agkurt <g.agkurt@hotmail.de>
2023-11-27 21:59:03 +0100

    add Todo List from data.js

--- components/todoList/index.js
+++ components/todoList/index.js
@@ -1,9 +1,14 @@
 import TodoItem from "../todoItem";
 
-export default function TodoList() {
+export default function TodoList({ todos }) {
   return (
-    <ul>
-      <TodoItem />
-    </ul>
+    <>
+      <h2>deine Todo Liste</h2>
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    </>
   );
 }
