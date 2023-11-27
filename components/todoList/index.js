dc364d3c2c40b83df302fd16fb4843df73fd2c22
Goekhan Agkurt <g.agkurt@hotmail.de>
2023-11-27 20:51:29 +0100

    add data.js file

---
+++ components/todoList/index.js
@@ -0,0 +1,9 @@
+import TodoItem from "../todoItem";
+
+export default function TodoList() {
+  return (
+    <ul>
+      <TodoItem />
+    </ul>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
+import { todos } from "@/lib/data";
 import GlobalStyle from "../styles";
 
 export default function App({ Component, pageProps }) {
+  console.log("das sind die todos:", todos);
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} todos={todos} />
     </>
   );
 }
